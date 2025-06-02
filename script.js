f8a4ce6dc5df5ad3cd48057be1126e30f733763d
Lucas-senac <zlsantana041@gmail.com>
2025-06-02 14:00:06 -0300

    atualização do html

---
+++ script.js
@@ -0,0 +1,13 @@
+// Olá mundo
+console.log('Ola, mundo!')
+
+// Função  resposavel por carregar o HTML e mostrar no Container
+function carregarPagina(pagina) {
+    fetch(pagina)
+        .then((response) => response.text())
+        .then((html) =>{
+            document.getElementById('content').innerHTML = html
+        })
+        .catch((error) => console.error("Erro ao carregar pagina: ",error))
+
+}                                                                                                                                                                                                                                   
